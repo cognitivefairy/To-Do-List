1e096c1010032ae14545626bb4efe0d17b928905
adeadesola <adeski17@gmail.com>
2026-09-11 12:08:36 +0100

    todo list done

--- list.py
+++ list.py
@@ -4,25 +4,28 @@
 
 todo_list = []
 
-def Add_list():
+# Add task function
+def add_task():
     todo_list
-    my_list.append("Wake up 5am")
-    print(my_list)
+    task_input = input("Input Task: ")
+    todo_list.append(task_input)
+    print(todo_list)
+
+# View task
+def view_task():
+    print(todo_list)
+
+# Delete task
+def delete_task():
+    task_remove = input("Input task to delete: ")
+    todo_list.remove(task_remove)
+    print(todo_list)
 
 
-def remove_task()
 
 
-
-
-
-
-
-
-
+# Main function
 def main():
-
-    Add_list()
    
     while True:
         print("1. Add task(s), 2. View task(s), 3. Delete tasks(s), 4. Quit")
@@ -32,13 +35,15 @@ def main():
 
         if user_input == "1":
             print("Add task(s)")
-            task_input = input("Write task: ")
+            add_task()
         elif user_input == "2":
             print("View task(s)")
+            view_task()
         elif user_input == "3":
             print("Delete tasks(s)")
+            delete_task()
         elif user_input == "4":
-            print("Quit")
+            print("Back to menu")
             break
         else:
             print ("Please input a number from 1 to 4")
